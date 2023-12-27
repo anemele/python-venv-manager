970af363be8a5959d2503f0e97b63262d607bdd4
Jason Swift <anemele@outlook.com>
2023-12-27 14:54:03 +0800

    correct types

--- pvm/cli.py
+++ pvm/cli.py
@@ -26,7 +26,7 @@ def ls():
 @click.argument("name", nargs=-1, required=True)
 @click.option("-v", "--version", help="specify the Python version (preinstalled)")
 @click.option("-f", "--force", is_flag=True, default=False, help="overwrite if exists")
-def add(name: tuple[str], version: str | None, force: bool):
+def add(name: tuple[str, ...], version: str | None, force: bool):
     logger.debug(f"{name=}")
     logger.debug(f"{version=}")
     logger.debug(f"{force=}")
@@ -36,7 +36,7 @@ def add(name: tuple[str], version: str | None, force: bool):
 
 @cli.command(help=remove.__doc__)
 @click.argument("name", nargs=-1, required=True)
-def rm(name: tuple[str]):
+def rm(name: tuple[str, ...]):
     logger.debug(f"{name=}")
     for it in name:
         remove(it)
@@ -45,7 +45,7 @@ def rm(name: tuple[str]):
 @cli.command(help=activate.__doc__)
 @click.argument("name", nargs=-1, required=True)
 @click.option("--pwsh", is_flag=True, default=False, help="use pwsh activator")
-def use(name: tuple[str], pwsh: bool):
+def use(name: tuple[str, ...], pwsh: bool):
     logger.debug(f"{name=}")
     logger.debug(f"{pwsh=}")
     for it in name:
